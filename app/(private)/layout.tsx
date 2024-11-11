25ef66dc8901a78c4b363202eede34b28361ec3a
Joshua Gonzales <soulepsycle1201@gmail.com>
2024-11-11 23:09:28 +0800

    chore: Refractor base on Prettier

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import AdminNavbar from './components/AdminNavbar'
+import React from "react";
+import AdminNavbar from "./components/AdminNavbar";
 
 const AdminLayout = ({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) => {
-  return (
-    <>
-        <div className="flex h-screen bg-gray-100">
-            <AdminNavbar />
-			{/* Main Content */}
-			<main className="flex-1 overflow-x-hidden overflow-y-auto">
-                {children}
-            </main>
-        </div>
-    </>
-  )
-}
+	return (
+		<>
+			<div className="flex h-screen bg-gray-100">
+				<AdminNavbar />
+				{/* Main Content */}
+				<main className="flex-1 overflow-x-hidden overflow-y-auto">
+					{children}
+				</main>
+			</div>
+		</>
+	);
+};
 
-export default AdminLayout
+export default AdminLayout;
